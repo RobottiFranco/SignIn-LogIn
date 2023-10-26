7f62dd4cf7c68b4de01017eb3c8427c79f8e607a
RobottiFranco <franco.robotti0@gmail.com>
2023-10-26 00:42:51 -0300

    add HTML and CSS

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+let form_login_register = document.querySelector("#forms-login-register")
+let form_login = document.querySelector("#login-form");
+let form_register = document.querySelector("#register-form");
+
+let background_login = document.querySelector("#")
+
+function registrar(){
+
+}
